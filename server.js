17499eddc20fd779b1fbbd17cae59d606b504cbe
krasotun <servrostov@gmail.com>
2022-10-12 22:34:10 +0300

    add fixes

--- server.js
+++ server.js
@@ -2,13 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
-
 app.use(express.static(path.join(__dirname, 'dist')));
-
-app.use('*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
 app.listen(PORT, () => {
 	console.log(`App works at http://localhost:${PORT}`);
 });
