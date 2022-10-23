363b9bfb1e6c52ba912d9450e9c44ccff6bb7f34
Marat <servrostov@gmail.com>
2022-10-23 17:44:14 +0300

    fix rendering

--- src/pages/main-page/main-page.template.ts
+++ src/pages/main-page/main-page.template.ts
@@ -1,4 +1,4 @@
 export default `
-form(template-props='signInForm' class='template-props')
 header(template-props='header' class='template-props')
+form(template-props='signInForm' class='template-props')
 `;
