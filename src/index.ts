3ee127c959e1c4806579456fe206afba8a825e3b
krasotun <servrostov@gmail.com>
2022-11-08 10:09:36 +0300

    fix remove not needed code

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,4 @@ import { pageToRender } from "./core/router";
 const root: HTMLElement | null = document.getElementById("root");
 document.addEventListener("DOMContentLoaded", () => {
   renderTemplateToDoM(pageToRender(), root as HTMLElement);
-  const wrapper = document.querySelector(".chat-search-form__input");
-  const onBlur = () => {
-    console.log("Blur");
-  };
-  const onFocus = () => {
-    console.log("Focus");
-  };
-  const onInput = () => {
-    console.log("input");
-  };
-  wrapper?.addEventListener("blur", onBlur);
-  wrapper?.addEventListener("focus", onFocus);
-  wrapper?.addEventListener("input", onInput);
 });
