48b47b2925cf5e6de9a6efc6ec25cb1665fc6874
krasotun <servrostov@gmail.com>
2022-11-09 13:00:43 +0300

    add service pages

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import { Router } from "./core/router";
 
 const router = new Router();
 document.addEventListener("DOMContentLoaded", () => {
-  router.use("/", ServicePage, {
+  router.use("/404", ServicePage, {
     errorCode: "404",
     errorText: "Страница не найдена",
+  });
+  router.use("/500", ServicePage, {
+    errorCode: "500",
+    errorText: "Ошибка сервера",
   });
   router.start();
 });
