58dd2b306454d24fbcbeada7e8222e9a090674f6
krasotun <servrostov@gmail.com>
2022-11-13 20:44:58 +0300

    add store

--- src/components/profile-data/profile-data.ts
+++ src/components/profile-data/profile-data.ts
@@ -8,11 +8,20 @@ import authController from "../../controllers/auth-controller";
 export class ProfileData extends Block {
   constructor(props: IProfileDataProps) {
     super(props);
+    authController.getUserInfo();
     store.on(StoreEvents.UPDATE, () => {
       const propsFromStore = store.getState();
       if (propsFromStore) {
         this.setProps(propsFromStore?.userInfo);
+        this.setProps({
+          first_name: "Marat",
+        });
       }
+    });
+    const newProps = store.getState();
+    console.log(newProps?.userInfo);
+    this.setProps({
+      first_name: "Marat",
     });
   }
   render() {
