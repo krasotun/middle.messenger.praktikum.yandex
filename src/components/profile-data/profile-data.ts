76f7938b83a4baf550bade734b09346c47e40f2b
krasotun <servrostov@gmail.com>
2022-11-13 14:24:46 +0300

    add props to data profile

--- src/components/profile-data/profile-data.ts
+++ src/components/profile-data/profile-data.ts
@@ -2,10 +2,17 @@ import { Block } from "../../core/block";
 import { IProfileDataProps } from "./profile-data.props";
 import { render as compileTemplate } from "pug";
 import template from "./profile-data.template";
+import store, { StoreEvents } from "../../core/store";
 
 export class ProfileData extends Block {
   constructor(props: IProfileDataProps) {
     super(props);
+    store.on(StoreEvents.UPDATE, () => {
+      const propsFromStore = store.getState();
+      if (propsFromStore) {
+        this.setProps(propsFromStore?.userInfo);
+      }
+    });
   }
   render() {
     const {
