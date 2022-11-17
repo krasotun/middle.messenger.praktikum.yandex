bb3a7fb601cb2eef0a3721a1dd43b53ab4f2b18d
krasotun <servrostov@gmail.com>
2022-11-17 10:19:24 +0300

    fix avatar swnding

--- src/core/http-client.ts
+++ src/core/http-client.ts
@@ -72,9 +72,10 @@ export class HTTPClient {
       if (withCredentials) {
         xhr.withCredentials = true;
       }
-      Object.keys(headers).forEach((key) => {
-        xhr.setRequestHeader(key, headers[key]);
-      });
+      // Object.keys(headers).forEach((key) => {
+      //   xhr.setRequestHeader(key, headers[key]);
+      // });
+      xhr.setRequestHeader("accept", "application/json");
       xhr.onload = () => {
         resolve(this._getRequestResult(xhr));
       };
