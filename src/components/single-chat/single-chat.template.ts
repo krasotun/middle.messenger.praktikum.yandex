f468c1100e60b77a9400daa409cbbfe6556c8c58
krasotun <servrostov@gmail.com>
2022-11-18 14:42:09 +0300

    add Добавил сингл чаты

--- src/components/single-chat/single-chat.template.ts
+++ src/components/single-chat/single-chat.template.ts
@@ -2,11 +2,34 @@ export default `
 li.single-chat
 	article.single-chat__container
 		.single-chat__left-block
-			img.single-chat__avatar(src=avatarLink alt=senderName)
-			.single-chat__message-data
-				p.single-chat__sender-name=senderName
-				p.single-chat__message-text=messageText
-			.single-chat__right-block
-				p.single-chat__message-time=messageTime
-				span.single-chat__unread-messages=unreadMessages
+				img.single-chat__avatar(src=avatarLink alt=senderName)
+				.single-chat__message-data
+					p.single-chat__title Название беседы
+					p.single-chat__sender-name=senderName
+					p.single-chat__message-text=messageText
+		.single-chat__right-block
+			p.single-chat__message-time=messageTime
+			span.single-chat__unread-messages=unreadMessages
+li.single-chat
+	article.single-chat__container
+		.single-chat__left-block
+				img.single-chat__avatar(src=avatarLink alt=senderName)
+				.single-chat__message-data
+					p.single-chat__title Название беседы
+					p.single-chat__sender-name=senderName
+					p.single-chat__message-text=messageText
+		.single-chat__right-block
+			p.single-chat__message-time=messageTime
+			span.single-chat__unread-messages=unreadMessages
+li.single-chat
+	article.single-chat__container
+		.single-chat__left-block
+				img.single-chat__avatar(src=avatarLink alt=senderName)
+				.single-chat__message-data
+					p.single-chat__title Название беседы
+					p.single-chat__sender-name=senderName
+					p.single-chat__message-text=messageText
+		.single-chat__right-block
+			p.single-chat__message-time=messageTime
+			span.single-chat__unread-messages=unreadMessages
 `;
