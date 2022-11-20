7ec42f3b8de555edf6e9e41ba8c28052c98aa8c6
krasotun <servrostov@gmail.com>
2022-11-20 18:01:56 +0300

    add bug with rendering

--- src/components/chat-window/chat-window.ts
+++ src/components/chat-window/chat-window.ts
@@ -3,17 +3,15 @@ import { IChatWindowProps } from "./chat-window.props";
 import { render as compileTemplate } from "pug";
 import template from "./chat-window.template";
 import store, { StoreEvents } from "../../core/store";
+import chatController from "../../controllers/chat-controller";
 export class ChatWindow extends Block {
   constructor(props: IChatWindowProps) {
     super(props);
     store.on(StoreEvents.UPDATE, () => {
       const activeChat = store.getState()?.activeChat;
-      if (activeChat) {
-        this.setProps({
-          activeChat,
-        });
-      }
+      this.setProps({ activeChat });
     });
+    chatController.getChatToken(activeChat);
   }
   render(): string {
     const { sendMessageForm, activeChat, userAddForm } = this.props;
