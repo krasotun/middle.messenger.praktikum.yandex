6de0b78313b96534499dd1891b314a516a4df6d7
krasotun <servrostov@gmail.com>
2022-11-22 12:16:51 +0300

    fix remove button

--- src/components/send-message-form/send-message-form.template.ts
+++ src/components/send-message-form/send-message-form.template.ts
@@ -1,6 +1,6 @@
 export default `
 form.form.send-message-form
-	button(template-props='attachButton' class='template-props')
+	// button(template-props='attachButton' class='template-props')
 	input(template-props='inputSendMessage' class='template-props')
 	button(template-props='submitButton' class='template-props')
 `;
