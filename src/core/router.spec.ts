2c7a88196d03cadfb1f811e26c8252934caca6fd
krasotun <servrostov@gmail.com>
2022-12-04 14:32:24 +0300

    add some tests

--- src/core/router.spec.ts
+++ src/core/router.spec.ts
@@ -1,13 +1,13 @@
 const assert = require("assert");
 const { JSDOM } = require("jsdom");
 
-describe("Проверяем переходы у Роута", () => {
+describe("#Router", () => {
   const { window } = new JSDOM(
     "<!DOCTYPE html><head></head><p>Stub document</p>",
     { url: "http://localhost/" }
   );
 
-  it("Переход на новую страницу должен менять состояние сущности history", () => {
+  it("Navigation to new page should change History", () => {
     window.history.pushState({ page: "login" }, "Login", "/login");
     window.history.pushState({ page: "register" }, "Register", "/register");
     assert.equal(window.history.length, 3);
